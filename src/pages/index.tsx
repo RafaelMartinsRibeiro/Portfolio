25a4f884a3fb3da0c922969b25274f29987a1ab4
RafaelMartinsRibeiro <rafaelmartins1964@hotmail.com>
2022-07-26 23:09:00 -0300

    Update Home page (index)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,35 @@
 import type { GetStaticPropsContext, NextPage } from "next";
 import { services } from "../../data";
+import ServiceCard from "../components/ServiceCard";
 
-const Home: NextPage = ({ services }: any) => {
+const Home: NextPage = () => {
   return (
-    <div>
-      <h1>hellow world</h1>
+    <div className="flex flex-col px-6 pt-1">
+      <h5 className="my-3 font-medium">
+        Entusiasta na área de tecnologia da informação, especialmente
+        desenvolvimento. Sou apaixonado por tecnologia desde muito cedo, e estou
+        constantemente aprimorando meus conhecimentos com a finalidade de me
+        tornar um profissional melhor. Meu principal objetivo é conseguir uma
+        oportunidade profissional para iniciar minha carreira no mundo de
+        desenvolvimento web.
+      </h5>
+
+      <div
+        className="p-4 mt-5 bg-gray-400"
+        style={{ marginLeft: "-1.5rem", marginRight: "-1.5rem" }}
+      >
+        <h6 className="my-3 font-bold text-xl tracking-wide">Soft skills</h6>
+        <div className="grid lg:grid-cols-2 gap-6">
+          {services.map((service) => (
+            <div
+              key={service.title}
+              className="lg:col-span-1 bg-gray-200 rounded-lg "
+            >
+              <ServiceCard service={service} />
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
