9236f6c20d4fee3de3c5ee8da9bae8884889a4c4
RafaelMartinsRibeiro <rafaelmartins1964@hotmail.com>
2022-08-01 21:43:33 -0300

    Added projects data

--- data.ts
+++ data.ts
@@ -9,7 +9,7 @@ import {
 
 import { BsCircleFill } from "react-icons/bs";
 
-import { ISkill, ISoftSkill } from "./type";
+import { IProject, ISkill, ISoftSkill } from "./type";
 
 export const softSkills: ISoftSkill[] = [
   {
@@ -96,5 +96,61 @@ export const tools: ISkill[] = [
     name: "VS Code",
     level: "90%",
     Icon: BsCircleFill,
+  },
+];
+
+export const projects: IProject[] = [
+  {
+    name: "FeedGet",
+    description:
+      "FeedGet é um aplicativo de feedbacks, desenvolvido 100% responsivo para ser integrado a qualquer aplicação web ou mobile. O usuário consegue além de escolher a categoria de seu feedback, também pode enviar uma mensagem personalizada junto a uma captura de tela automática.",
+    imagePath: "/images/feedget.png",
+    deployedUrl: "https://feedget-2lnvobk5d-rafaelmartinsribeiro.vercel.app/",
+    githubUrl: "https://github.com/RafaelMartinsRibeiro/FeedGet",
+    category: ["react"],
+    techs: ["React", "TypeScript", "TailwindCSS", "Vite"],
+  },
+  {
+    name: "Move.it",
+    description:
+      "O Move.it consiste em um contador de 25 minutos, quando o contador finaliza o tempo é mostrado um desafio de relaxamento corporal ou visual, onde o usuário pode ou não completa-lo. Para incentivar o usuário a efetuar os exercicios de relaxamento foi adicionado um sistema de leveling, ou seja quando o usuario completa determinada atividade o mesmo recebe uma quantia de xp, quando ele atinge determinado montante de xp ele adquire um nível, fornecendo ao usuario um sentimento de recompensa",
+    imagePath: "/images/moveit.png",
+    deployedUrl: "https://moveit-gamma-seven.vercel.app/",
+    githubUrl: "https://github.com/RafaelMartinsRibeiro/MoveIt",
+    category: ["next"],
+    techs: ["NextJS", "TypeScript", "js-cookie"],
+  },
+  {
+    name: "W Forte",
+    description:
+      "O projeto consiste em uma aplicação web 100% responsiva, que foi criado com a finalidade de exibir as informações do local, como: estrutura, modalidades, professores, local, etc.",
+    imagePath: "/images/wforte.png",
+    deployedUrl: "https://w-forte.vercel.app/",
+    githubUrl: "https://github.com/RafaelMartinsRibeiro/W-Forte",
+    category: ["next"],
+    techs: [
+      "NextJS",
+      "TypeScript",
+      "TailwindCSS",
+      "Frame Motion",
+      "Headless UI",
+    ],
+  },
+  {
+    name: "LetMeAsk",
+    description:
+      "O LetMeAsk consiste em uma aplicação web que tem como finalidade a criação de salas Q&A. O LetMeAsk tem a proposta de resolver problemas de acumulo de perguntas durante uma live, transmissão, etc. Com ela você pode criar salas privadas onde somente os que possuem o ID da sala podem acessar, os participantes podem fazer perguntas(desde que estejam logados com o Google), votar nas mesmas. Já o criador da sala pode demarcar as perguntas como respondidas, colocar foco, dentre outras funcionalidades...",
+    imagePath: "/images/letmeask.png",
+    deployedUrl: "https://letmeask-93071.web.app/",
+    githubUrl: "https://github.com/RafaelMartinsRibeiro/LetMeAsk",
+    category: ["react"],
+    techs: [
+      "React",
+      "TypeScript",
+      "Sass",
+      "Firebase",
+      "react-hot-toast",
+      "Vite",
+    ],
   },
 ];
