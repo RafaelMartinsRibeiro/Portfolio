336ddcac5a99dd71d96f5a785122b7b0d49d1acd
RafaelMartinsRibeiro <rafaelmartins1964@hotmail.com>
2022-08-03 22:49:25 -0300

    Added animations in Bar component

--- src/components/Bar.tsx
+++ src/components/Bar.tsx
@@ -1,20 +1,40 @@
+import { motion } from "framer-motion";
 import { FunctionComponent } from "react";
+import { GiSpring } from "react-icons/gi";
 import { ISkill } from "../../type";
 
 const Bar: FunctionComponent<{ data: ISkill }> = ({
   data: { Icon, level, name },
 }) => {
+  const variants = {
+    initial: {
+      width: 0,
+    },
+    animate: {
+      width: level,
+      transition: {
+        type: "spring",
+        damping: 10,
+        stiffness: 100,
+        duration: 0.4,
+      },
+    },
+  };
+
   return (
     <div className="text-white my-2 bg-gray-300 rounded-full dark:bg-dark-200">
-      <div
+      <motion.div
         style={{
           width: level,
         }}
         className="px-4 py-1 flex items-center rounded-full bg-gradient-to-r from-purple-600 to-blue-500"
+        variants={variants}
+        initial="initial"
+        animate="animate"
       >
         <Icon className="mr-3" />
         {name}
-      </div>
+      </motion.div>
     </div>
   );
 };
