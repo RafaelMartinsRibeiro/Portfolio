9eddd2dcab7e28cae1594b4cc29c96aafc5e7dd6
RafaelMartinsRibeiro <rafaelmartins1964@hotmail.com>
2022-08-19 20:52:20 -0300

    Update

--- data.ts
+++ data.ts
@@ -102,6 +102,17 @@ export const tools: ISkill[] = [
 export const projects: IProject[] = [
   {
     id: 1,
+    name: "Redux Cart",
+    description:
+      "Este projeto consiste em uma aplicação web 100% responsiva que tem como objetivo simular um carrinho de compras online feito com redux.",
+    imagePath: "/images/reduxCart.png",
+    deployedUrl: "https://redux-cart-psi.vercel.app/",
+    githubUrl: "https://github.com/RafaelMartinsRibeiro/ReduxCart",
+    category: ["React"],
+    techs: ["React", "Redux Toolkit", "Axios", "Vite"],
+  },
+  {
+    id: 2,
     name: "FeedGet",
     description:
       "FeedGet é um aplicativo de feedbacks, desenvolvido 100% responsivo para ser integrado a qualquer aplicação web ou mobile. O usuário consegue além de escolher a categoria de seu feedback, também pode enviar uma mensagem personalizada junto a uma captura de tela automática.",
@@ -112,7 +123,7 @@ export const projects: IProject[] = [
     techs: ["React", "TypeScript", "TailwindCSS", "Vite"],
   },
   {
-    id: 2,
+    id: 3,
     name: "Move.it",
     description:
       "O Move.it consiste em um contador de 25 minutos, quando o contador finaliza o tempo é mostrado um desafio de relaxamento corporal ou visual, onde o usuário pode ou não completa-lo. Para incentivar o usuário a efetuar os exercicios de relaxamento foi adicionado um sistema de leveling, ou seja quando o usuario completa determinada atividade o mesmo recebe uma quantia de xp, quando ele atinge determinado montante de xp ele adquire um nível, fornecendo ao usuario um sentimento de recompensa",
@@ -123,7 +134,7 @@ export const projects: IProject[] = [
     techs: ["NextJS", "TypeScript", "js-cookie"],
   },
   {
-    id: 3,
+    id: 4,
     name: "W Forte",
     description:
       "O projeto consiste em uma aplicação web 100% responsiva, que foi criado com a finalidade de exibir as informações do local, como: estrutura, modalidades, professores, local, etc.",
@@ -140,7 +151,7 @@ export const projects: IProject[] = [
     ],
   },
   {
-    id: 4,
+    id: 5,
     name: "LetMeAsk",
     description:
       "O LetMeAsk consiste em uma aplicação web que tem como finalidade a criação de salas Q&A. O LetMeAsk tem a proposta de resolver problemas de acumulo de perguntas durante uma live, transmissão, etc. Com ela você pode criar salas privadas onde somente os que possuem o ID da sala podem acessar, os participantes podem fazer perguntas(desde que estejam logados com o Google), votar nas mesmas. Já o criador da sala pode demarcar as perguntas como respondidas, colocar foco, dentre outras funcionalidades...",
@@ -158,16 +169,6 @@ export const projects: IProject[] = [
     ],
   },
   {
-    id: 5,
-    name: "OnePiece MemoryGame",
-    description: "Jogo da memoria com a temática de One Piece",
-    imagePath: "/images/memorygame.png",
-    deployedUrl: "https://onepiece-memory-game.vercel.app/",
-    githubUrl: "https://github.com/RafaelMartinsRibeiro/memoryGame",
-    category: ["JavaScript"],
-    techs: ["JavaScript", "HTML", "CSS"],
-  },
-  {
     id: 6,
     name: "Formulário de cadastro",
     description: "Formulário simples de cadastro com suas devidas validações",
@@ -179,6 +180,16 @@ export const projects: IProject[] = [
   },
   {
     id: 7,
+    name: "OnePiece MemoryGame",
+    description: "Jogo da memoria com a temática de One Piece",
+    imagePath: "/images/memorygame.png",
+    deployedUrl: "https://onepiece-memory-game.vercel.app/",
+    githubUrl: "https://github.com/RafaelMartinsRibeiro/memoryGame",
+    category: ["JavaScript"],
+    techs: ["JavaScript", "HTML", "CSS"],
+  },
+  {
+    id: 8,
     name: "MusicPlayer",
     description: "Player de musica",
     imagePath: "/images/musicplayer.png",
@@ -188,7 +199,7 @@ export const projects: IProject[] = [
     techs: ["JavaScript", "HTML", "CSS"],
   },
   {
-    id: 8,
+    id: 9,
     name: "Tic-Tac-Toe",
     description: "Jogo da velha",
     imagePath: "/images/tictactoe.png",
